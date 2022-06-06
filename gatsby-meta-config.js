773aa140b29d9540f27bc32e87b661b4a1716f19
hyunchanjung <tmzkdlqmfn1@naver.com>
2022-06-06 17:10:54 +0900

    Initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `HyunChan Jung`,
+  description: `HyunChan Jung Development Blog`,
+  author: `HyunChan Jung`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: `https://hyunchan-jung.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `hyunchan-jung`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/sloth.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: 'disqus_Fo7hFArZ2l', // Your disqus-short-name. check disqus.com.
+    utterances: 'hyunchan-jung/hyunchan-jung.github.io.comment', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'hyunchanjung',
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+
+}
